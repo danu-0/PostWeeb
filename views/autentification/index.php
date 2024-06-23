10fd919cafd08a341a8ebce7d435393db780f1e4
danu-0 <danuhaeridaputra07@gmail.com>
2024-06-23 16:04:57 +0800

    login page

---
+++ views/autentification/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Login</title>
+    <link rel="stylesheet" href="../../css/autentification.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="login-form">
+            <h2>Login</h2>
+            <p>Silahkan login dengan akun anda</p>
+            <form action="../dashboard/index.php" method="POST">
+                <input type="text" id="username" name="username" placeholder="Username" required>
+                <input type="password" id="password" name="password" placeholder="Password" required>
+                <div class="remember-me">
+                    <input type="checkbox" id="remember" name="remember">
+                    <label for="remember">Ingat Saya?</label>
+                </div>
+                <button type="submit" action=>Login</button>
+            </form>
+            <p>Belum punya akun? <a href="./register.php">Daftar</a></p>
+        </div>
+        <div class="info-box">
+            <div class="image-box">
+                <div class="box-content">
+                <div class="slide-left">
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                        
+                </div>
+                <div class="slide-right">
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                       
+                </div>
+                <div class="slide-left">
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                      
+                </div>
+                <div class="slide-right">
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                       
+                </div>
+                <div class="slide-left">
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                      
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ views/autentification/register.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Register</title>
+    <link rel="stylesheet" href="../../css/autentification.css">
+</head>
+<body>
+    <div class="login-container">
+    <div class="info-box">
+            <div class="image-box">
+                <div class="box-content">
+                <div class="slide-right">
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                        
+                </div>
+                <div class="slide-left">
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                       
+                </div>
+                <div class="slide-right">
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                      
+                </div>
+                <div class="slide-left">
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                       
+                </div>
+                <div class="slide-right">
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                        <h1>ANJAY POST ANJAY POST ANJAY POST ANJAY POS</h1>
+                      
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="login-form">
+            <h2>Login</h2>
+            <p>Silahkan login dengan akun anda</p>
+            <form action="../dashboard/index.php" method="POST">
+                <input type="text" id="username" name="username" placeholder="Username" required>
+                <input type="email" id="email" name="email" placeholder="Email" required>
+                <input type="password" id="password" name="password" placeholder="Password" required>
+                <button type="submit" action=>Daftar</button>
+            </form>
+            <p>Sudah punya akun? <a href="./index.php">Login</a></p>
+        </div>
+      
+    </div>
+</body>
+
+</html>
